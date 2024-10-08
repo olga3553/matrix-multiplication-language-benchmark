f2c902190b32b2f5883c7cc23f283ce616f83b3c
Olga Kalisiak <01178853@pw.edu.pl>
2024-10-08 15:36:22 +0100

    changed cpu and memory calculation

--- Python/matrix_python/matrix_python.py
+++ Python/matrix_python/matrix_python.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 import psutil
+import time
 
 def matrix_multiply(a, b):
     n = len(a)
@@ -18,15 +19,26 @@ def setup_matrices():
     B = np.random.rand(size, size)
     return A, B
 
-@pytest.mark.benchmark(min_rounds=5)
+@pytest.mark.benchmark(min_rounds=1)
 def test_matrix_multiply(benchmark, setup_matrices):
     A, B = setup_matrices
+
+    # Memory and CPU before
+    process = psutil.Process()
+    cpu_start = time.process_time()
+    mem_before = process.memory_info().rss / (1024 ** 2)  # in MB
+
     result = benchmark(matrix_multiply, A, B)
+
+    # Memory and CPU after
+    cpu_end = time.process_time()
+    mem_after = process.memory_info().rss / (1024 ** 2)  # in MB
+
+    cpu_time_ms = (cpu_end - cpu_start) * 1000  # in ms
+    mem_usage = max(mem_after - mem_before, 0)
+
+    print()
+    print(f"Czas procesora: {cpu_time_ms:.2f} ms")
+    print(f"Zużycie pamięci: {mem_usage:.2f} MB")
+
     assert result is not None
-
-
-size = 512
-A = np.random.rand(size, size)
-B = np.random.rand(size, size)
-print("CPU usage:", psutil.cpu_percent(), "%")
-print("Memory usage:", psutil.virtual_memory().percent, "%")
